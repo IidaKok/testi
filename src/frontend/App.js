0c797bf60b4cf353e9e871914eee380a6d14648e
IidaKok <iida.kokkonen@edu.savonia.fi>
2023-03-15 13:51:33 +0200

    navbar added

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -3,16 +3,20 @@ import { Login } from "./components/Login";
 import { Register } from "./components/Register";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import { Logged } from "./pages/UserHome";
+import { SeriesBrowser } from "./components/Browser";
+import { NavBar } from "./components/NavBar";
 
 const App = () => {
     const [user, setUser] = useState("");
 
     return (
         <Router>
+            {user ? <NavBar/> : null}
             <Routes>
                 <Route path="/" element={
                 user ? <Logged user={user}/> : <Login  saveUser={setUser}/>} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/series" element={<SeriesBrowser />} />
             </Routes>
         </Router>
     )
