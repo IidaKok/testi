5b3e6bc688ddfb2d720bc561d14f3b18673e17b5
IidaKok <iida.kokkonen@edu.savonia.fi>
2023-03-27 18:23:23 +0300

    hh

--- src/backend/App.js
+++ src/backend/App.js
@@ -14,13 +14,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //allows cors
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
 app.use("/api/users", userRoutes);
 app.use("/api/bookseries", seriesRoutes);
