c94f761b37a33af67215ca85ea5153cbebcdc4a5
IidaKok <iida.kokkonen@edu.savonia.fi>
2023-03-27 18:19:44 +0300

    .

--- src/backend/App.js
+++ src/backend/App.js
@@ -14,13 +14,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //allows cors
 app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Content-Type", "application/json");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Content-Type", "application/json");
   next();
 });
 
