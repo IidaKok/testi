8678c662b1f128ea01fa16f81339fdbf4f778fe1
IidaKok <iida.kokkonen@edu.savonia.fi>
2023-03-27 18:18:56 +0300

    app.js change

--- src/backend/App.js
+++ src/backend/App.js
@@ -3,28 +3,32 @@ const bodyParser = require("body-parser");
 
 const userRoutes = require("./routes/user-routes");
 const seriesRoutes = require("./routes/series-routes");
+const bookRoutes = require("./routes/book-routes");
 const bookshelfRoutes = require("./routes/bookshelf-routes");
+const bookcopyRoutes = require("./routes/bookcopy-routes");
+
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //allows cors
 app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Content-Type", "application/json");
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Content-Type", "application/json");
+  next();
+});
 
 app.use("/api/users", userRoutes);
 app.use("/api/bookseries", seriesRoutes);
+app.use("/api/book", bookRoutes);
 app.use("/api/bookshelf", bookshelfRoutes);
-
+app.use("/api/bookcopy", bookcopyRoutes);
 app.use((error, req, res, next) => {
     if (res.headerSent) {
         return next(error);
@@ -32,5 +36,25 @@ app.use((error, req, res, next) => {
     res.status(error.code || 500);
     res.json({ message: error.message || "unknown error" });
 })
+
+app.post("/bookcopy", (req, res) => {
+    const q = "INSERT INTO bookcopy (`bookname`, `edition`, `publicationyear`, `purchaseprice`, `purchasedate`, `condition`, `description`, `solddate`, `soldprice`) VALUES (?)";
+    const values = [
+        req.body.bookname,
+        req.body.edition,
+        req.body.publicationyear,
+        req.body.purchaseprice,
+        req.body.purchasedate,
+        req.body.condition,
+        req.body.description,
+        req.body.solddate,
+        req.body.soldprice,
+    ];
+
+    db.query(q, [values], (err, data) => {
+        if (err) return res.json(err);
+        return res.json("Book has been created successfully")
+    });
+});
 
 app.listen(5000);
