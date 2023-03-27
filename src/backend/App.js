dcccb923dbfdf4dbee41d0406385894e60bebe07
IidaKok <iida.kokkonen@edu.savonia.fi>
2023-03-27 18:22:34 +0300

    ..

--- src/backend/App.js
+++ src/backend/App.js
@@ -19,8 +19,6 @@ app.use(function(req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Content-Type", "application/json");
   next();
 });
 
