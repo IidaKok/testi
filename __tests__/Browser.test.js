f1dbb02a8aa33b0dc14aa0b4444cae2611d8af5f
Hans00T <hans.tervakoski@edu.savonia.fi>
2023-04-12 11:01:30 +0300

    .

--- __tests__/Browser.test.js
+++ __tests__/Browser.test.js
@@ -3,42 +3,61 @@
  */
 
 import React, { Component } from "react";
-import { render, fireEvent, screen, within } from '@testing-library/react';
+import { render, fireEvent, screen, within, getByTestId } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from 'react-router-dom';
 // import React from 'react';
 import { SeriesBrowser, SeriesInfo, BookInfo } from '../src/frontend/components/Browser';
+import "@testing-library/jest-dom/extend-expect";
+import fetchMock from 'jest-fetch-mock';
+
+fetchMock.enableMocks();
+
+const user = { iduser: 1, username: "Matti.Mainio", password: "Kissa", email: "matti@email.com" };
+jest.setTimeout(10000);
 
 describe("Browser", () => {
     test('clicking the "+" button adds all books for the user logged in', () => {
-        let mockUser = { iduser: 1 };
-        const { getByText } = render(<SeriesBrowser user={mockUser} />, { wrapper: BrowserRouter });
-        const addButton = getByText('+');
-    
+        render(
+            <BrowserRouter>
+                <SeriesBrowser user={user} />
+            </BrowserRouter>
+        );
+        const addButton = getByTestId('add-books');
+
         fireEvent.click(addButton);
-    
+
         expect(setBookToAdd).toHaveBeenCalledWith(expectedBookSeriesId);
         expect(insertBooksToBookshelf).toHaveBeenCalled();
     });
-    
+
     test('clicking the "Edit" button opens the edit modal', () => {
         const mockSeries = { idbookseries: 1, bookseries: 'Mock Series', publisher: 'Mock Publisher' };
-        const { getByText } = render(<SeriesBrowser />);
-        const editButton = getByText('Edit');
-    
+        render(
+            <BrowserRouter>
+                <SeriesBrowser user={user} />
+            </BrowserRouter>
+        );
+        const editButton = getByTestId('edit-series');
+
         fireEvent.click(editButton);
-    
+
         expect(openEditModal).toHaveBeenCalledWith(mockSeries);
     });
-    
+
     test('the table is rendered with the correct data', () => {
         const mockSeries = [
             { idbookseries: 1, bookseries: 'Mock Series 1', publisher: 'Mock Publisher 1' },
             { idbookseries: 2, bookseries: 'Mock Series 2', publisher: 'Mock Publisher 2' },
         ];
-        const { getAllByRole } = render(<SeriesBrowser />);
+        render(
+            <BrowserRouter>
+                <SeriesBrowser user={user} />
+            </BrowserRouter>
+        );
+        setSeries(mockSeries);
         const tableRows = getAllByRole('row');
-    
+
         expect(tableRows).toHaveLength(mockSeries.length + 1); // Header row + data rows
         expect(tableRows[1]).toHaveTextContent('Mock Series 1');
         expect(tableRows[1]).toHaveTextContent('Mock Publisher 1');
@@ -49,25 +68,32 @@ describe("Browser", () => {
 
 describe("SeriesInfo", () => {
     test('clicking the "+" button adds the selected book for the user logged in', () => {
-        let mockUser = { iduser: 1 };
-        const { getByText } = render(<SeriesInfo user={mockUser} />, { wrapper: BrowserRouter });
-        const addButton = getByText('+');
+        render(
+            <BrowserRouter>
+                <SeriesInfo user={user} />
+            </BrowserRouter>
+        );
+        const addButton = getByTestId('add-book');
 
         fireEvent.click(addButton);
-        const confirmButton = getByText('Confirm')
+        const confirmButton = getByTestId('confirm');
         fireEvent.click(confirmButton);
-    
+
         expect(setBookToAdd).toHaveBeenCalledWith(expectedBookId);
         expect(insertBook).toHaveBeenCalled();
     });
-        
+
     test('clicking the "Edit" button opens the edit modal', () => {
         const mockSeries = { idbookseries: 1, bookseries: 'Mock Series', publisher: 'Mock Publisher' };
-        const { getByText } = render(<SeriesInfo />);
-        const editButton = getByText('Edit');
-    
+        render(
+            <BrowserRouter>
+                <SeriesInfo user={user} />
+            </BrowserRouter>
+        );
+        const editButton = getByTestId('edit-series');
+
         fireEvent.click(editButton);
-    
+
         expect(openEditModal).toHaveBeenCalledWith(mockSeries);
     });
 
@@ -76,9 +102,14 @@ describe("SeriesInfo", () => {
             { idbook: 1, bookname: 'Mock Book 1', publicationyear: '2000' },
             { idbook: 2, bookname: 'Mock Book 2', publicationyear: '2001' },
         ];
-        const { getAllByRole } = render(<SeriesInfo />);
+        render(
+            <BrowserRouter>
+                <SeriesInfo user={user} />
+            </BrowserRouter>
+        );
+        setBookData(mockBooks);
         const tableRows = getAllByRole('row');
-    
+
         expect(tableRows).toHaveLength(mockBooks.length + 1); // Header row + data rows
         expect(tableRows[1]).toHaveTextContent('Mock Book 1');
         expect(tableRows[1]).toHaveTextContent('2000');
@@ -89,35 +120,45 @@ describe("SeriesInfo", () => {
 
 describe("BookInfo", () => {
     test('clicking the "Quick add" button adds the selected book for the user logged in', () => {
-        let mockUser = { iduser: 1 };
-        const { getByText } = render(<SeriesInfo user={mockUser} />, { wrapper: BrowserRouter });
-        const addButton = getByText('Quick add');
+        render(
+            <BrowserRouter>
+                <BookInfo user={user} />
+            </BrowserRouter>
+        );
+        const addButton = getByTestId('quick-add');
 
         fireEvent.click(addButton);
-        const confirmButton = getByText('Confirm')
+        const confirmButton = getByTestId('confirm');
         fireEvent.click(confirmButton);
-    
+
         expect(setBookToAdd).toHaveBeenCalledWith(expectedBookId);
         expect(insertBook).toHaveBeenCalled();
     });
 
     test('clicking the "Add with information" button opens a modal for adding a book copy with information', () => {
-        let mockUser = { iduser: 1 };
-        const { getByText } = render(<SeriesInfo user={mockUser} />, { wrapper: BrowserRouter });
-        const addButton = getByText('Add with information');
+        render(
+            <BrowserRouter>
+                <BookInfo user={user} />
+            </BrowserRouter>
+        );
+        const addButton = getByTestId('add-w-info');
 
         fireEvent.click(addButton);
-        
+
         expect(openModal).toHaveBeenCalled();
     });
 
     test('clicking the "Edit" button opens an edit modal', () => {
-        const { getByText } = render(<BookInfo />);
-        const editButton = getByText('Edit');
-    
+        render(
+            <BrowserRouter>
+                <BookInfo user={user} />
+            </BrowserRouter>
+        );
+        const editButton = getByTestId('edit-book');
+
         fireEvent.click(editButton);
-    
+
         expect(openEditModal).toHaveBeenCalled();
     });
-})
+});
 
