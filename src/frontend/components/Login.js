d92e68675cb55dc4c43aaf7b7d1570dc04eeda7f
IidaKok <iida.kokkonen@edu.savonia.fi>
2023-04-21 16:56:53 +0300

    login and app change

--- src/frontend/components/Login.js
+++ src/frontend/components/Login.js
@@ -1,54 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../Loginstyle.css";
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
 
 const Login = (props) => {
-
-    const { saveUser } = props;
+    const { userLogged } = props;
 
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
-    const [query, setQuery] = useState("");
-    const [users, setUsers] = useState([]);
-    const [buttonPressed, setButtonPressed] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
     const [errorMessage2, setErrorMessage2] = useState("");
     const [invalidName, setInvalidName] = useState(false);
     const [invalidPassword, setInvalidPassword] = useState(false);
 
-
-    useEffect(() => {
-        console.log(query);
-        const fetchUsers = async () => {
-            await fetch("http://localhost:5000/api/users/" + query)
-                .then((res) => res.json())
-                .then((data) => {
-                    if (data.message === undefined) {
-                        saveUser({ username: data.username, password: data.password, email: data.email, iduser: data.iduser });
-                        setInvalidName(false);
-                        setInvalidPassword(false);
-                        return;
-                    }
-                    let msg = data.message;
-                    console.log("Message: ", msg);
-                    
-                    if (msg.includes("Password")) {
-                        setErrorMessage2(msg);
-                        setInvalidPassword(true);
-                    }
-                    else if (msg.includes("Username")) {
-                        setErrorMessage(msg);
-                        setInvalidName(true);
-                    }
-                });
-        }
-        if (query !== "") fetchUsers();
-    }, [query]);
-
     const handleSubmit = (event) => {
         event.preventDefault();
-        setButtonPressed(true);
         setErrorMessage("");
         setErrorMessage2("");
         setInvalidName(false);
@@ -63,22 +29,54 @@ const Login = (props) => {
             setErrorMessage2("Password can't be empty");
         }
         else {
-            let q = [];
-            if (name !== "") q.push(name);
-            if (password !== "") q.push(password);
-            setQuery(q.join("&")); 
+            fetch('http://localhost:5000/login', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: name,
+                    password: password
+                }),
+                credentials: 'include'
+            })
+                .then(response => {
+                    if (response.ok) {
+                        setInvalidName(false);
+                        setInvalidPassword(false);
+                        userLogged(true);
+                    }
+                    else {
+                        const s = response.json()
+                        s.then((data) => {
+                            console.log(data.message);
+                            if (data.message.includes("Password")) {
+                                setErrorMessage2(data.message);
+                                setInvalidPassword(true);
+                            }
+                            else if (data.message.includes("Username")) {
+                                setErrorMessage(data.message);
+                                setInvalidName(true);
+                            }
+                        });
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                });
         }
     }
+    
     return (
         <div className="Forms">
             <h2>Login</h2>
             <div className="Container">
                 <form onSubmit={(e) => handleSubmit(e)}>
-                    <input type="text" value={name} className={invalidName ? "invalid" : "valid"} onChange={(e) => setName(e.target.value)} placeholder="Username..." />
-                    <p>{errorMessage}</p>
-                    <input type="password" value={password} className={invalidPassword ? "invalid" : "valid"} onChange={(e) => setPassword(e.target.value)} placeholder="Password..." />
-                    <p>{errorMessage2}</p>
-                    <input type="submit" value="Login" />
+                    <input type="text" name="username" value={name} className={invalidName ? "invalid" : "valid"} onChange={(e) => setName(e.target.value)} placeholder="Username..." />
+                    <p data-testid="nameError">{errorMessage}</p>
+                    <input name="password" type="password" value={password} className={invalidPassword ? "invalid" : "valid"} onChange={(e) => setPassword(e.target.value)} placeholder="Password..." />
+                    <p data-testid="passError">{errorMessage2}</p>
+                    <input data-testid="logBtn" type="submit" value="Login" />
                 </form>
             </div>
 
