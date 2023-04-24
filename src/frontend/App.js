476164df23f262ea27e7238884b3fa70c38cbfc4
IidaKok <iida.kokkonen@edu.savonia.fi>
2023-04-24 11:25:15 +0300

    app.js routing change

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -3,78 +3,85 @@ import { Login } from "./components/Login";
 import { Register } from "./components/Register";
 import { Routes, Route, HashRouter as Router } from "react-router-dom";
 import { Logged } from "./pages/UserHome";
-import { SeriesBrowser } from "./components/Browser";
-import { SeriesInfo } from "./components/BrowserSeriesInfo";
-import { BookInfo } from "./components/BrowserBookInfo";
+import { SeriesBrowser, SeriesInfo, BookInfo } from "./components/Browser";
 import { NavBar } from "./components/NavBar";
 import { UserPage } from "./components/UserPage";
 import { Addbook } from "./components/Addbook";
 import { Addseries } from "./components/Addseries";
 import { Update } from "./components/Update";
 import { EditPhotos } from "./components/EditPhotos";
+import { Email } from "./components/sendEmail";
+import { ChangePassword } from "./components/changePassword";
+import headerPicture from "./header_picture.jpg";
 
 const App = () => {
-    const [user, setUser] = useState("");
-    const [logged, setLogged] = useState(null);
+    const [user, setUser] = useState(null);
+    const [logged, setLogged] = useState(false);
+    const [authCheckCompleted, setAuthCheckCompleted] = useState(false);
 
     useEffect(() => {
         const fetchUser = () => {
-             try {
-                 fetch('http://localhost:5000/', {
-                     credentials: 'include',
-                 })
-                     .then((res) => res.json())
-                     .then((data) => {
-                         console.log("data: ", data);
-                         if (data.loggedIn) {
-                             setLogged(true);
-                             console.log("data.user: ", data.user[0])
-                             setUser(data.user[0]);
-                         }
-                         if (!data.loggedIn) {
-                             setLogged(false);
-                         }
-                     })
-             } catch (error) {
-                 console.error(error);
-             }
-         };
+            fetch('http://localhost:5000/api/users/', {
+                credentials: 'include',
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log("data: ", data);
+                    if (data.loggedIn) {
+                        setLogged(true);
+                        console.log("data.user: ", data.user[0])
+                        setUser(data.user[0]);
+                    }
+                    if (!data.loggedIn) {
+                        setLogged(false);
+                        setUser("");
+                    }
+                    setAuthCheckCompleted(true);
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        };
         fetchUser();
     }, [logged]);
 
+    if (!logged) {
+        return (
+            <Router>
+                {authCheckCompleted && (
+                    <Routes>
+                        <Route path="/" element={<Login userLogged={setLogged} />} />
+                        <Route path="/register" element={<Register />} />
+
+                        <Route path="/sendEmail" element={<Email />} />
+                        <Route path="/changePassword/:token" element={<ChangePassword />} />
+                    </Routes>
+                )}
+            </Router>
+        )
+    }
+
     return (
         <Router>
-            {logged ? <NavBar userLogged={setLogged} /> : null}
-            <Routes>
-                <Route path="/" element={
-                    logged ? <Logged user={user} /> : <Login userLogged={setLogged} />} />
-                <Route path="/register" element={<Register />} />
-
-                <Route path="/series" element={
-                    logged ? <SeriesBrowser user={user} /> : <Login userLogged={setLogged} />} />
-
-                <Route path="/series/books/:idbookseries" element={
-                    logged ? <SeriesInfo user={user} /> : <Login userLogged={setLogged} />} />
-
-                <Route path="/series/books/book/:idbook" element={
-                    logged ? <BookInfo user={user} /> : <Login userLogged={setLogged} />} />
-
-                <Route path="/userPage" element={
-                    logged ? <UserPage user={user} /> : <Login userLogged={setLogged} />} />
-
-                <Route path="/Addbook" element={
-                    logged ? <Addbook user={user} /> : <Login userLogged={setLogged} />} />
-
-                <Route path="/Addseries" element={
-                    logged ? <Addseries /> : <Login userLogged={setLogged} />} />
-
-                <Route path="/update/:idbookshelf" element={
-                    logged ? <Update user={user} /> : <Login userLogged={setLogged} />} />
-
-                <Route path="/EditPhotos/:idbookshelf" element={
-                    logged ? <EditPhotos user={user} /> : <Login userLogged={setLogged} />} />
-
-            </Routes>
+            <div className="header">
+                <img src={headerPicture} style={{ width: "100%", filter: "blur(2px)" }} />
+                <h1 className="header-text">Group B Book Archive</h1>
+            </div>
+            <NavBar user={user} userLogged={setLogged} />
+            {authCheckCompleted && (
+                <Routes>
+                    <Route path="/" element={<Logged user={user} islogged={logged} />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/series" element={<SeriesBrowser user={user} />} />
+                    <Route path="/series/books/:idbookseries" element={<SeriesInfo user={user} />} />
+                    <Route path="/series/books/book/:idbook" element={<BookInfo user={user} />} />
+                    <Route path="/userPage" element={<UserPage user={user} />} />
+                    <Route path="/Addbook" element={<Addbook user={user} />} />
+                    <Route path="/Addseries" element={<Addseries />} />
+                    <Route path="/update/:idbookshelf" element={<Update user={user} />} />
+                    <Route path="/EditPhotos/:idbookshelf" element={<EditPhotos user={user} />} />
+                </Routes>
+            )}
         </Router>
     )
 }
